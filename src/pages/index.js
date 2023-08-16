d0355c799698c5c866965445f1cae6936ee7d2e3
arikaps <arikaps18@gmail.com>
2023-08-16 20:39:25 +0700

    commit7

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
     <Layout title= 'DashboardPage'>
       <main>
         <p>
-          DashboardPage Revisi satuuuuuuuuu
+          DashboardPage Revisi nammmm
         </p>
       </main>
           </Layout>
