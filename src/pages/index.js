2c36f7b30f5f51e4a14dfc2a30451b9c0c65cd58
arikaps <arikaps18@gmail.com>
2023-08-16 13:22:26 +0700

    TA4

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
     <Layout title= 'DashboardPage'>
       <main>
         <p>
-          DashboardPage Revisi 111111111
+          DashboardPage Revisi ke semua
         </p>
       </main>
           </Layout>
